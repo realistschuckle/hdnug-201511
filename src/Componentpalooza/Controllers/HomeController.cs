105b8f2eca799f6b4965e73d8f7464ca99bb42ee
Curtis Schlak <realistschuckle@gmail.com>
2015-11-11 09:50:11 -0600

    Cleaner and soberer.

--- src/Componentpalooza/Controllers/HomeController.cs
+++ src/Componentpalooza/Controllers/HomeController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
 namespace Componentpalooza.Controllers
@@ -10,21 +6,8 @@ namespace Componentpalooza.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            ViewData["Title"] = "Calendar!";
+            return View("Index");
         }
 
         public IActionResult Error()
--- src/Componentpalooza/gulpfile.js
+++ src/Componentpalooza/gulpfile.js
@@ -1,47 +1,71 @@
 /// <binding Clean='clean' />
 'use strict';
-var gulp = require("gulp"),
-  del = require("del"),
-  concat = require("gulp-concat"),
-  cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify"),
-  project = require("./project.json");
+var gulp = require('gulp'),
+  path = require('path'),
+  del = require('del'),
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat'),
+  cssmin = require('gulp-cssmin'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  project = require('./project.json');
 
 var paths = {
-  webroot: "./" + project.webroot + "/"
+  webroot: './' + project.webroot + '/'
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.jssrc = 'Assets/js/**/*.js';
+paths.csssrc = 'Assets/css/**/*.scss';
+paths.js = paths.webroot + 'js/**/*.js';
+paths.minJs = paths.webroot + 'js/**/*.min.js';
+paths.css = paths.webroot + 'css/**/*.css';
+paths.minCss = paths.webroot + 'css/**/*.min.css';
+paths.concatJsDest = paths.webroot + 'js/site.min.js';
+paths.concatCssDest = paths.webroot + 'css/site.min.css';
 
-gulp.task("clean:js", function() {
+gulp.task('clean:js', function() {
   return del(paths.concatJsDest);
 });
 
-gulp.task("clean:css", function() {
+gulp.task('clean:css', function() {
   return del(paths.concatCssDest);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task('compile:js', function () {
+  return gulp.src(paths.jssrc)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest(path.join(paths.webroot, 'js')));
+});
 
-gulp.task("min:js", function() {
-  gulp.src([paths.js, "!" + paths.minJs], {
-      base: "."
+gulp.task('compile:css', function () {
+  return gulp.src(paths.csssrc)
+    .pipe(sass())
+    .pipe(gulp.dest(path.join(paths.webroot, 'css')));
+})
+
+gulp.task('min:js', ['compile:js'], function() {
+  gulp.src([paths.js, '!' + paths.minJs], {
+      base: '.'
     })
     .pipe(concat(paths.concatJsDest))
     .pipe(uglify())
-    .pipe(gulp.dest("."));
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task("min:css", function() {
-  gulp.src([paths.css, "!" + paths.minCss])
+gulp.task('min:css', ['compile:css'], function() {
+  gulp.src([paths.css, '!' + paths.minCss])
     .pipe(concat(paths.concatCssDest))
     .pipe(cssmin())
-    .pipe(gulp.dest("."));
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task('watch', ['compile'], function () {
+  gulp.watch(paths.jssrc, ['min:js']);
+  gulp.watch(paths.csssrc, ['min:css']);
+});
+
+gulp.task('clean', ['clean:js', 'clean:css']);
+gulp.task('compile', ['compile:js', 'compile:css']);
+gulp.task('min', ['min:js', 'min:css']);
