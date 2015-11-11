f83d32488d2206f2ac95cc49a8fe94d56b30f641
Curtis Schlak <realistschuckle@gmail.com>
2015-11-11 04:24:28 -0600

    Use Dapper and Npgsql in the app as an example.

--- src/Componentpalooza/Startup.cs
+++ src/Componentpalooza/Startup.cs
@@ -5,9 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Npgsql;
+using Dapper;
 
 namespace Componentpalooza
 {
+		public class User
+		{
+				public int Id { get; set; }
+				public string FirstName { get; set; }
+				public string LastName { get; set; }
+				public string Email { get; set; }
+				public DateTime InsertedAt { get; set; }
+
+				public override string ToString()
+				{
+						return LastName + ", " + FirstName;
+				}
+		}
+
     public class Startup
     {
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
@@ -19,10 +35,14 @@ namespace Componentpalooza
         {
             // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
-            
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+								using (var conn = new NpgsqlConnection("Host=localhost;Username=postgres;Password=postgres;Database=polyvox_dev"))
+								{
+										var people = conn.Query<User>("select id as Id, first_name as FirstName, last_name as LastName from users");
+										await context.Response.WriteAsync(string.Format("Hello {0}!!!", string.Join("\n\t", people)));
+								}
             });
         }
     }
