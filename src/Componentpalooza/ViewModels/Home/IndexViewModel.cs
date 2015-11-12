f90b9cdd23866651886cc5040379636a96c93b39
Curtis Schlak <realistschuckle@gmail.com>
2015-11-12 09:47:57 -0600

    Untabifying...

--- src/Componentpalooza/ViewModels/Home/IndexViewModel.cs
+++ src/Componentpalooza/ViewModels/Home/IndexViewModel.cs
@@ -9,66 +9,66 @@ namespace Componentpalooza.ViewModels.Home
     {
         public IndexViewModel(IEnumerable<Appointment> appointments)
         {
-						_appointments = appointments;
+            _appointments = appointments;
         }
 
-				public IEnumerable<IGrouping<DateTime, Appointment>> Past
-				{
-						get
-						{
-								return _appointments
-										.OrderByDescending(appt => appt.StartsAt.Date)
-										.ThenBy(appt => appt.StartsAt.TimeOfDay)
-										.Where(appt => appt.StartsAt <= DateTime.Now)
-										.GroupBy(appt => appt.StartsAt.Date, appt => appt);
-						}
-				}
+        public IEnumerable<IGrouping<DateTime, Appointment>> Past
+        {
+            get
+            {
+                return _appointments
+                    .OrderByDescending(appt => appt.StartsAt.Date)
+                    .ThenBy(appt => appt.StartsAt.TimeOfDay)
+                    .Where(appt => appt.StartsAt <= DateTime.Now)
+                    .GroupBy(appt => appt.StartsAt.Date, appt => appt);
+            }
+        }
 
-				public IEnumerable<IGrouping<DateTime, Appointment>> Future
-				{
-						get
-						{
-								return _appointments
-										.OrderBy(appt => appt.StartsAt)
-										.Where(appt => appt.StartsAt > DateTime.Now)
-										.GroupBy(appt => appt.StartsAt.Date, appt => appt);
-						}
-				}
+        public IEnumerable<IGrouping<DateTime, Appointment>> Future
+        {
+            get
+            {
+                return _appointments
+                    .OrderBy(appt => appt.StartsAt)
+                    .Where(appt => appt.StartsAt > DateTime.Now)
+                    .GroupBy(appt => appt.StartsAt.Date, appt => appt);
+            }
+        }
 
-				public string FormatDate(DateTime dt)
-				{
-						return dt.ToString("d");
-				}
+        public string FormatDate(DateTime dt)
+        {
+            return dt.ToString("d");
+        }
 
-				public string FormatTime(DateTime dt)
-				{
-						return dt.ToString("t");
-				}
+        public string FormatTime(DateTime dt)
+        {
+            return dt.ToString("t");
+        }
 
-				public string FormatLength(int num)
-				{
-						switch(num)
-						{
-								case 1:
-										return "15 mins";
-								case 2:
-										return "30 mins";
-								case 3:
-										return "45 mins";
-								case 4:
-										return "1 hour";
-								case 5:
-										return "1.25 hours";
-								case 6:
-										return "1.5 hours";
-								case 7:
-										return "1.75 hours";
-								case 8:
-										return "2 hours";
-						}
-						return "Forever!";
-				}
+        public string FormatLength(int num)
+        {
+            switch(num)
+            {
+                case 1:
+                    return "15 mins";
+                case 2:
+                    return "30 mins";
+                case 3:
+                    return "45 mins";
+                case 4:
+                    return "1 hour";
+                case 5:
+                    return "1.25 hours";
+                case 6:
+                    return "1.5 hours";
+                case 7:
+                    return "1.75 hours";
+                case 8:
+                    return "2 hours";
+            }
+            return "Forever!";
+        }
 
-				private readonly IEnumerable<Appointment> _appointments;
-		}
+        private readonly IEnumerable<Appointment> _appointments;
+    }
 }
